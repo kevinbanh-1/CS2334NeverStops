0792b092682da31f31c85faf7cad2d74cea1c16d
kevin banh <kevin.n.banh-1@ou.edu>
2020-10-21 16:54:07 -0500

    SuperStack more methods + tests

--- src/SuperStack.java
+++ src/SuperStack.java
@@ -3,25 +3,35 @@ public class SuperStack<T> {
 	private T[] items;
 	private int top;
 	
-	
-	public SuperStack<T>(int size){
+
+	@SuppressWarnings("unchecked")
+	public SuperStack(int size) {
+		// TODO Auto-generated constructor stub
 		items = (T[]) new Object[size];
-		
 	}
-	
+
 	public boolean isEmpty() {
-		return true;
+		return top == 0;
 	}
 	
 	public boolean isFull() {
-		return true;
+		return top == items.length;
 	}
 	
 	public boolean push(T item) {
+		if(isFull()) {
+			return false;
+		}
+		items[top] = item;
 		return true;
 	}
 	
 	public T pop() {
-		return null;
+		if(isEmpty()) {
+			return null;
+		}
+		else {
+			return items[top];
+		}
 	}
 }
---
+++ src/SuperStackTest.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SuperStackTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testIsEmpty() {
+		// Create an empty stack and check if it's empty
+		// add a few elements and check if it's empty
+		SuperStack<Integer> stack = new SuperStack<Integer>(1);
+		boolean result = stack.isEmpty();
+		assertTrue(result);
+	}
+
+	@Test
+	void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPush() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
